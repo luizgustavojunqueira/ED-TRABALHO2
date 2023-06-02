f1d4e62dbce0be970b4f1759da9825bbe5a61926
PacMan111 <luizgustavossj@gmail.com>
2023-06-02 18:48:43 -0300

    adicionado contagem de tempo da arvore AVL

--- main.cpp
+++ main.cpp
@@ -1,41 +1,69 @@
 #include "abb.h"
+#include "avl.h"
 #include <random>
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[])
+{
 
-    int inc = 100;
-    int step = 100;
-    int max = 1000;
+    int inc = atoi(argv[2]);
+    int step = atoi(argv[3]);
+    int max = atoi(argv[4]);
+    int rept = atoi(argv[5]);
+    int max_num = atoi(argv[6]);
 
-    for(inc; inc <= max; inc += step){
-        int rept = 10;
-        double mediaTempo = 0;
-        for(rept; rept > 0; rept--){
-            int v[inc];
-            for(int i = 0; i < inc; i++){
-                v[i] = rand() % 10000;
+    for (int i = inc; i <= max; i += step)
+    {
+
+        double mediaTempoABB = 0;
+        double mediaTempoAVL = 0;
+
+
+        for (int j = 0; j < rept; j++)
+        {
+
+            int v[i];
+            for (int k = 0; k < i; k++)
+            {
+                v[k] = rand() % max_num;
             }
 
-            clock_t inicio = clock();
+            clock_t inicioABB = clock();
 
             ABB abb;
-            for(int i = 0; i < inc; i++){
-                abb.insere(v[i]);
+
+            for (int k = 0; k < i; k++)
+            {
+                abb.insere(v[k]);
             }
 
             abb.printar();
 
-            mediaTempo += (double)(clock() - inicio)/CLOCKS_PER_SEC;
+            mediaTempoABB += (double)(clock() - inicioABB) / CLOCKS_PER_SEC;
 
-            
             abb.~ABB();
+
+            clock_t inicioAVL = clock();
+
+            AVL avl;
+
+            for (int k = 0; k < i; k++)
+            {
+                avl.insere(v[k]);
+            }
+
+            mediaTempoAVL += (double)(clock() - inicioAVL) / CLOCKS_PER_SEC;
+
+            avl.~AVL();
         }
-        
-        cout << inc <<" numeros, demorou: " << fixed << mediaTempo << setprecision(20) << endl;
-        
-        mediaTempo = mediaTempo / rept;
+
+        mediaTempoABB = mediaTempoABB / rept;
+        mediaTempoAVL = mediaTempoAVL / rept;
+
+        cout << i <<" numeros. ABB demorou: " << fixed << mediaTempoABB << setprecision(20) << " AVL demorou: " << fixed << mediaTempoAVL << setprecision(20) <<endl;
     }
+
 }
